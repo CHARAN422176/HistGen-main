fd7eabec970e3e73a5251a4fd102a478563fc1aa
CHARAN422176 <422176@student.nitandhra.ac.in>
2025-06-22 13:54:44 +0530

    commit

--- models/histgen_model.py
+++ models/histgen_model.py
@@ -19,52 +19,16 @@ class HistGenModel(nn.Module):
         params = sum([np.prod(p.size()) for p in model_parameters])
         return super().__str__() + '\nTrainable parameters: {}'.format(params)
 
-    # def forward_pathology(self, images, targets=None, mode='train', update_opts={}):
-        
-    #     att_feats = self.wsi_mapping(images)
-    #     fc_feats = torch.mean(att_feats, dim=1)
-        
-    #     if mode == 'train':
-    #         output = self.encoder_decoder(fc_feats, att_feats, targets, mode='forward')
-    #         return output
-    #     elif mode == 'sample':
-    #         output, output_probs = self.encoder_decoder(fc_feats, att_feats, mode='sample')
-    #         return output
-    #     else:
-    #         raise ValueError
     def forward_pathology(self, images, targets=None, mode='train', update_opts={}):
-    # images shape: (batch_size, sequence_length, hidden_size)
-    
-    num_regions = self.args.num_regions  # Make sure you have this in your args
-    region_size = self.args.region_size  # Make sure you have this in your args
-    
-    # Transform into regions (reshape)
-    hidden_states_reshape = transform_tokens2regions(images, num_regions, region_size)
-    # hidden_states_reshape shape: (batch_size * num_regions, region_size, hidden_size)
-
-    # Flatten or pool region tokens to get 2D tensor (N, in_features)
-    # Option A: Flatten region tokens (recommended if linear layer expects 1024 or 768 inputs)
-    input_linear = hidden_states_reshape.view(hidden_states_reshape.size(0), -1)  # shape (N, region_size * hidden_size)
-    
-    # If the input feature size doesn't match wsi_mapping's expected input (768 or 1024),
-    # adjust your region_size or hidden_size accordingly
-    
-    # Pass to linear layer
-    att_feats = self.wsi_mapping(input_linear)  # shape (batch_size * num_regions, d_vf)
-
-    # Reshape att_feats back to (batch_size, num_regions, d_vf)
-    batch_size = images.size(0)
-    att_feats = att_feats.view(batch_size, num_regions, -1)
-    
-    # Compute mean pooled features (fc_feats)
-    fc_feats = torch.mean(att_feats, dim=1)  # shape (batch_size, d_vf)
-    
-    if mode == 'train':
-        output = self.encoder_decoder(fc_feats, att_feats, targets, mode='forward')
-        return output
-    elif mode == 'sample':
-        output, output_probs = self.encoder_decoder(fc_feats, att_feats, mode='sample')
-        return output
-    else:
-        raise ValueError
-
+        
+        att_feats = self.wsi_mapping(images)
+        fc_feats = torch.mean(att_feats, dim=1)
+        
+        if mode == 'train':
+            output = self.encoder_decoder(fc_feats, att_feats, targets, mode='forward')
+            return output
+        elif mode == 'sample':
+            output, output_probs = self.encoder_decoder(fc_feats, att_feats, mode='sample')
+            return output
+        else:
+            raise ValueError
